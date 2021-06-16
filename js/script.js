c70e80baeec2d970e1671e65acdc05e7549ed72d
DAISSY <84278476+daissy3@users.noreply.github.com>
2021-06-17 04:46:59 +0900

    Add files via upload
    second update

--- js/script.js
+++ js/script.js
@@ -1,12 +1,12 @@
 $(window).on('scroll', function () {
-    if( $(window).scrollTop() > 100 && $(window).scrollTop() <600 ){
+    if( $(window).scrollTop() > 200  ){
         $('.mainText').addClass('mainTextOpacity');
     }else{
         $('.mainText').removeClass('mainTextOpacity');
     }
 });
 $(window).on('scroll', function () {
-    if( $(window).scrollTop() > 700 && $(window).scrollTop() <1300 ){
+    if( $(window).scrollTop() > 900  ){
         $('.mainText2').addClass('mainTextOpacity2');
     }else{
         $('.mainText2').removeClass('mainTextOpacity2');
@@ -23,3 +23,19 @@ $(window).on('scroll', function () {
         $('.cartIcon').removeClass('vsd');
     }
 });
+
+$('.countSel').on('click', function () {
+    if( $('.countryList').css('display') === 'none'){
+        $('.countryList').show();
+    }else{
+        $('.countryList').hide();
+     }
+ });
+
+ $('.langSel').on('click', function () {
+    if( $('.languageList').css('display') === 'none'){
+        $('.languageList').show();
+    }else{
+        $('.languageList').hide();
+     }
+ });
